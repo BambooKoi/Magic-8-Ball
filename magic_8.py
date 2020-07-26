9cda14664987bece2d551c8d8b96113da5a90758
BambooKoi <67773378+BambooKoi@users.noreply.github.com>
2020-07-25 22:58:50 -0400

    Initial commit

---
+++ magic_8.py
@@ -0,0 +1,44 @@
+import random
+
+answers = [
+    'It is certain',
+    'It is decidedly so',
+    'Yes',
+    'Reply hazy try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outlook not good',
+    'Very doubtful',
+]
+
+
+def help():
+    print('###')
+    print('\nHelp/Instructions:\n')
+    print('When the program first runs, the Magic 8 Ball will ask the user if')
+    print('they would like to ask a question.')
+    print('The user can then enter \'Y\' for yes & proceed to ask a question.')
+    print('Or the user can enter \'N\' for no to exit the program.\n')
+    print('Entering \'Help\' at any point will result in this screen.')
+    print('###')
+
+while True:  # This allows the user to ask again.
+    print('\nWould you like to ask the Magic 8 Ball something? ')
+    question = input('Enter \'Y\' to ask a question or \'N\' to quit. ')
+    if question.lower() == 'y':
+        question = input('\nWhat is your question? ')
+        if 'meaning of life' in question.lower():
+            print('Magic 8 Ball says: "42"')
+        elif question.lower() == 'help':
+            help()
+        else:
+            print(f'\n> Magic 8 Ball says: "{random.choice(answers)}"')
+    elif question.lower() == 'n':
+        break
+    elif question.lower() == 'help':
+        help()
+        continue
+    else:
+        print(f'\nSorry, I wasn\'t able to understand "{question}".')
+        print('Enter \'Help\' for help.')
