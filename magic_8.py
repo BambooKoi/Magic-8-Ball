d69b5adcef2caa03f4bd6272d8125d93e5e915a2
Barbara Nguyen <67773378+BambooKoi@users.noreply.github.com>
2020-08-18 16:36:48 -0400

    Simplified asking process
    Revised the Help Screen to reflect changes.

--- magic_8.py
+++ magic_8.py
@@ -1,6 +1,22 @@
 import os
 import random
 
+
+def help():
+    os.system('cls')
+    print('//Help & Instructions:')
+    print('Users can ask the Magic 8 Ball any yes-or-no question')
+    print('by typing out the question and hitting the (ENTER KEY)')
+    print('\nEntering \'Quit\' will exit Magic 8 Ball.')
+    print('Entering \'Help\' at any point will result in this screen.')
+
+    print('\n//About Magic 8 Ball')
+    print('This is a digital Magic 8-Ball made with Python.')
+    print('The orignal Magic 8-Ball was a billard\'s 8-ball used for fortune telling.')
+    print('Users would ask a yes/no question and the Magic 8-Ball would give a prediction.')
+
+    input('\nPress (ENTER KEY) to continue. ')
+
 answers = [
     'It is certain',
     'It is decidedly so',
@@ -18,34 +34,19 @@ answers = [
     'Don\'t count on it'
 ]
 
-
-def help():
+while True:
     os.system('cls')
-    print('\n###')
-    print('Help/Instructions:\n')
-    print('When the program first runs, the Magic 8 Ball will ask the user if')
-    print('they would like to ask a yes or no question.')
-    print('The user can then enter \'Y\' for yes & proceed to ask a question.')
-    print('Or the user can enter \'N\' for no to exit the program.\n')
-    print('Entering \'Help\' at any point will result in this screen.')
-    print('###')
-
-while True:  # This allows the user to ask again.
-    print('\nWould you like to ask the Magic 8 Ball something? ')
-    question = input('Enter \'Y\' to ask a question or \'N\' to quit. ')
-    if question.lower() == 'y':
-        question = input('\nWhat is your question? ')
-        if 'meaning of life' in question.lower():
-            print('Magic 8 Ball says: "42"')
-        elif question.lower() == 'help':
-            help()
-        else:
-            print(f'\n> Magic 8 Ball says: "{random.choice(answers)}"')
-    elif question.lower() == 'n':
+    print('//THE MAGIC 8-BALL')
+    print('Type \'help\' for help or \'quit\' to quit.')
+    question = input('\nWhat would you like to ask the Magic 8 Ball?\n> ')
+    if question.lower() == 'quit':
         break
     elif question.lower() == 'help':
         help()
         continue
+    elif 'meaning of life' in question.lower():
+        print('Magic 8 Ball says: "42"')
+        input('\nPress (ENTER KEY) to continue. ')
     else:
-        print(f'\nSorry, I wasn\'t able to understand "{question}".')
-        print('Enter \'Help\' for help.')
+        print(f'\nMagic 8 Ball says: "{random.choice(answers)}"')
+        input('\nPress (ENTER KEY) to continue. ')
